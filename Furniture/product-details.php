beb45edd7358ef2ed7eb1575f05dbefb61e9c28f
Maxzen <vinuthnakumarmaxzen08@gmail.com>
2024-12-27 11:52:39 +0530

    Update product-details.php

--- Furniture/product-details.php
+++ Furniture/product-details.php
@@ -4,7 +4,7 @@ session_start();
 // Database connection
 $servername = "localhost";
 $username = "root";
-$password = "1432";
+$password = "";
 $dbname = "rental_website";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
