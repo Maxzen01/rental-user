3d8b577e62c1473cfd2e78b24825cfb859fe571c
Maxzen <vinuthnakumarmaxzen08@gmail.com>
2024-12-27 12:01:52 +0530

    Update add_to_cart.php

--- sports/add_to_cart.php
+++ sports/add_to_cart.php
@@ -3,7 +3,7 @@ session_start();
 
 $servername = "localhost";
 $username = "root";
-$password = "1432";
+$password = "";
 $dbname = "rental_website";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
